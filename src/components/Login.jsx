f020bc09de999ba1e4d360243cceafabb661f34a
Niamh Brockbank <niamh.brockbank@openbet.com>
2024-12-06 14:27:54 +0000

    Combine input values into one state object

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,7 +1,24 @@
+import { useState } from "react";
+
 export default function Login() {
+  const [inputValues, setInputValues] = useState({
+    email: "",
+    password: "",
+  });
+
   function handleSubmit(event) {
     event.preventDefault();
   }
+
+  function handleInputChange(identifier, value) {
+    setInputValues((previousValues) => {
+      return {
+        ...previousValues,
+        [identifier]: value,
+      };
+    });
+  }
+
   return (
     <form>
       <h2>Login</h2>
@@ -9,12 +26,24 @@ export default function Login() {
       <div className="control-row">
         <div className="control no-margin">
           <label htmlFor="email">Email</label>
-          <input id="email" type="email" name="email" />
+          <input
+            onChange={(e) => handleInputChange("email", e.target.value)}
+            id="email"
+            type="email"
+            name="email"
+            value={inputValues.email}
+          />
         </div>
 
         <div className="control no-margin">
           <label htmlFor="password">Password</label>
-          <input id="password" type="password" name="password" />
+          <input
+            onChange={(e) => handleInputChange("password", e.target.value)}
+            id="password"
+            type="password"
+            name="password"
+            value={inputValues.password}
+          />
         </div>
       </div>
 
